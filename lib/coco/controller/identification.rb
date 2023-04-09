c8be17ba4af681bc3a6523a26bf369f88c17bf8d
Krisna Pranav <krisna.pranav@gmail.com>
2023-04-09 17:21:36 +0530

    controller: identification

--- lib/coco/controller/identification.rb
+++ lib/coco/controller/identification.rb
@@ -1,25 +1,28 @@
 module Coco
     module Base
-        module Identification
+      module Identification
+  
+        module_function
+  
+        @ppid = ::Process.pid
 
-            module_function
-
-            @ppid = ::Process.pid
-
-            def pid
-                process_pid = ::Process.pid
-                if @ppid != process_pid
-                    @pid = nil
-                    @ppid = process_pid
-                end
-                @pid ||= SecureRandom.urlsafe_base64.tap { |str| @prefix_len = str.length }
-            end
-
-            def target2uuid(target) 
-                return nil unless target.start_with? pid
-                target[@prefix_len..-1].to_i
-            end
-
+        def pid
+           process_pid = ::Process.pid
+           if @ppid != process_pid
+              @pid = nil
+              @ppid = process_pid
+           end
+           @pid ||= SecureRandom.urlsafe_base64.tap { |str| @prefix_len = str.length }
         end
+
+        def target2uuid(target)
+           return nil unless target.start_with? pid
+           target[@prefix_len..-1].to_i
+        end
+  
+        def target2pid(target)
+           target ? target[0..(@prefix_len - 1)] : Coco.app_name
+        end
+      end
     end
-end
+  end
