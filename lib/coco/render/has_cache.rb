38d16e3e3082e85ab209ea17e9b0612f69053c4f
Krisna Pranav <krisna.pranav@gmail.com>
2023-04-09 20:02:19 +0530

    coco: render[has_cache]

---
+++ lib/coco/render/has_cache.rb
@@ -0,0 +1,33 @@
+module Plezi
+    module Base
+       module HasCache
+          def self.extended(base)
+             base.instance_variable_set :@_lock, Mutex.new
+             base.instance_variable_set :@_cache, {}.dup
+          end
+ 
+          def store(key, value)
+             @_lock.synchronize { @_cache[key] = value }
+          end
+          alias []= store
+
+          def get(key)
+             @_lock.synchronize { @_cache[key] }
+          end
+          alias [] get
+       end
+
+       module HasStore
+
+          def store(key, value)
+             (Thread.current[(@_chache_name ||= object_id.to_s(16))] ||= {}.dup)[key] = value
+          end
+          alias []= store
+
+          def get(key)
+             (Thread.current[(@_chache_name ||= object_id.to_s(16))] ||= {}.dup)[key]
+          end
+          alias [] get
+       end
+    end
+ end
