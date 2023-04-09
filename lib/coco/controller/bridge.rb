a803b5d2c4bfa1c16c8c3da39d8d702cfcaa8eed
Krisna Pranav <krisna.pranav@gmail.com>
2023-04-09 13:29:04 +0530

    controller: bridge

---
+++ lib/coco/controller/bridge.rb
@@ -0,0 +1,31 @@
+module Coco
+    module Base
+        module Bridge
+            CONTROLLER_NAME = "coco.controller".to_sym
+            CLIENT_NAME = "@_pl__client".to_sym
+
+            def controller client
+                client.env[CONTROLLER_NAME]
+            end
+
+            def on_open client
+                c = controller(client)
+                c.instance_variable_set(CLIENT_NAME, client)
+                if client.protocol == :sse
+                    c.on_sse
+                else
+                    c.on_open
+                end
+            end
+
+            def on_message client, data
+                controller(client).on_message(data)
+            end
+
+            def on_shutdown client
+                controller(client).on_shutdown
+            end
+            
+        end
+    end
+end
