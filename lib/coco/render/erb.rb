97834afccfaf63dde794ec6673b0438755f97045
Krisna Pranav <krisna.pranav@gmail.com>
2023-04-09 20:16:49 +0530

    render: erb

--- lib/coco/render/erb.rb
+++ lib/coco/render/erb.rb
@@ -1,30 +1,33 @@
 require 'coco/render/has_cache' unless defined? ::Coco::Base::HasStore
 module Coco
-    module Base
-        module RenderERB
-            extend ::Coco::Base::HasStore
+   module Base
+      module RenderERB
+         extend ::Coco::Base::HasStore
 
-            module_function
+         module_function
 
-            def call(filename, context, &block)
-                return unless defined? ::ERB
-                return unless File.exist?(filename)
-                engine = load_engine(filename)
-                engine.result(context, &block)
+         def call(filename, context, &block)
+            return unless defined? ::ERB
+            return unless File.exist?(filename)
+            engine = load_engine(filename)
+            engine.result(context, &block)
+         end
+         if ENV['RACK_ENV'.freeze] == 'production'.freeze
+            def load_engine(filename)
+               engine = self[filename]
+               return engine if engine
+               self[filename] = ::ERB.new(::Coco.try_utf8!(IO.binread(filename)))
             end
-
-            if ENV['RACK_ENV'.freeze] == 'production'.freeze 
-                def load_engine(filename)
-                    engine = self[filename]
-                    return engine if engine 
-                    self[filename] = ::ERB.new(::Coco.try_utf8!(IO.binread(filename)))
-                end
-            else 
-                def load_engine(filename)
-                end
-                
-
-        end
-    end
-
+         else
+            def load_engine(filename)
+               engine, tm = self[filename]
+               return engine if engine && (tm == File.mtime(filename))
+               self[filename] = [(engine = ::ERB.new(::Coco.try_utf8!(IO.binread(filename)))), File.mtime(filename)]
+               engine
+            end
+         end
+      end
+   end
 end
+
+::Coco::Renderer.register :erb, ::Coco::Base::RenderERB
