1c3692e2acf88b2e3c55c872dc5296ca1df20100
Krisna Pranav <krisna.pranav@gmail.com>
2023-04-09 13:29:59 +0530

    coco: controller[bridge]

--- lib/coco/controller/bridge.rb
+++ lib/coco/controller/bridge.rb
@@ -1,31 +1,39 @@
 module Coco
     module Base
-        module Bridge
-            CONTROLLER_NAME = "coco.controller".to_sym
-            CLIENT_NAME = "@_pl__client".to_sym
+      module Bridge
+        CONTROLLER_NAME = "coco.controller".to_sym
+        CLIENT_NAME = "@_pl__client".to_sym 
 
-            def controller client
-                client.env[CONTROLLER_NAME]
-            end
-
-            def on_open client
-                c = controller(client)
-                c.instance_variable_set(CLIENT_NAME, client)
-                if client.protocol == :sse
-                    c.on_sse
-                else
-                    c.on_open
-                end
-            end
-
-            def on_message client, data
-                controller(client).on_message(data)
-            end
-
-            def on_shutdown client
-                controller(client).on_shutdown
-            end
-            
+        def controller client
+          client.env[CONTROLLER_NAME]
         end
+  
+        def on_open client
+          c = controller(client)
+          c.instance_variable_set(CLIENT_NAME, client)
+          if client.protocol == :sse
+            c.on_sse
+          else
+            c.on_open
+          end
+        end
+
+        def on_message client, data
+           controller(client).on_message(data)
+        end
+
+        def on_shutdown client
+           controller(client).on_shutdown
+        end
+
+        def on_close client
+           controller(client).on_close
+        end
+
+        def on_drained client
+           controller(client).on_drained
+        end
+        extend self
+      end
     end
-end
+  end
