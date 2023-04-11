120b64503aef038240330c57aa4680c5a30816b5
Krisna Pranav <krisna.pranav@gmail.com>
2023-04-11 20:03:38 +0530

    render: slim

--- lib/coco/render/slim.rb
+++ lib/coco/render/slim.rb
@@ -1,25 +1,33 @@
 require 'coco/render/has_cache' unless defined? ::Coco::Base::HasStore
 module Coco
-    module Base
-        module RenderSlim
-            extend ::Coco::Base::HasStore
+   module Base
+      module RenderSlim
+         extend ::Coco::Base::HasStore
 
-            def call(filename, context, &block)
-                return unless defined? ::Slim
-                return unless File.exist?(filename)
-                engine = load_engine(filename)
-                engine.render(context.receiver, &block)
+         module_function
+
+         def call(filename, context, &block)
+            return unless defined? ::Slim
+            return unless File.exist?(filename)
+            engine = load_engine(filename)
+            engine.render(context.receiver, &block)
+         end
+         if ENV['RACK_ENV'.freeze] == 'production'.freeze
+            def load_engine(filename)
+               engine = self[filename]
+               return engine if engine
+               self[filename] = (Slim::Template.new { ::Coco.try_utf8!(IO.binread(filename)) })
             end
-
-            if ENV['RACK_ENV'.freeze] == 'production'.freeze
-                def load_engine(filename)
-                end
-            else
-                def load_engine(filename)
-                end
-        end
-    end
+         else
+            def load_engine(filename)
+               engine, tm = self[filename]
+               return engine if engine && (tm == File.mtime(filename))
+               self[filename] = [(engine = Slim::Template.new { ::Coco.try_utf8!(IO.binread(filename)) }), File.mtime(filename)]
+               engine
+            end
+         end
+      end
+   end
 end
 
-
-::Coco::Render.register :slim, ::Coco::Base::RenderSlim
+::Coco::Renderer.register :slim, ::Coco::Base::RenderSlim
